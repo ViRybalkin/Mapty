2d4b4f10d18b05554632e044a8f936b4d6439647
Vitaliy <kinjeks@gmail.com>
2021-08-15 19:08:35 +0300

    Refactoring to OOP

--- script.js
+++ script.js
@@ -22,42 +22,73 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    pos => {
-      const { latitude } = pos.coords;
-      const { longitude } = pos.coords;
+class App {
+  #map;
+  #mapEvent;
+  constructor() {
+    this._getPosition();
+    form.addEventListener('submit', this._newWorkout.bind(this));
+    inputType.addEventListener('change', this._toggleElevationField.bind(this));
+  }
 
-      const coords = [latitude, longitude];
-
-      const map = L.map('map').setView(coords, 14);
-
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      map.on('click', mapEvent => {
-        const { lat, lng } = mapEvent.latlng;
-        const coords = [lat, lng];
-        L.marker(coords)
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 50,
-              maxHeight: 50,
-              className: 'cycling-popup',
-              autoClose: false,
-              closeOnClick: false,
-            })
-          )
-          .setPopupContent('WorkOut')
-          .openPopup();
+  _getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), () => {
+        console.log("don't get position");
       });
-    },
-    () => {
-      console.log("don't get position");
     }
-  );
+  }
+  _loadMap(pos) {
+    const { latitude } = pos.coords;
+    const { longitude } = pos.coords;
+
+    const coords = [latitude, longitude];
+
+    this.#map = L.map('map').setView(coords, 14);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+  _toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+  _newWorkout(e) {
+    e.preventDefault();
+
+    inputDistance.value =
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+
+    const { lat, lng } = this.#mapEvent.latlng;
+    const coords = [lat, lng];
+    L.marker(coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 50,
+          maxHeight: 50,
+          className: 'cycling-popup',
+          autoClose: false,
+          closeOnClick: false,
+        })
+      )
+      .setPopupContent('WorkOut')
+      .openPopup();
+  }
 }
+
+const app = new App();
